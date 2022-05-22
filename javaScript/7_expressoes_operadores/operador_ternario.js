543d601c44740f21e7d5a4b513eab0aa26064cb6
Eric <yuukisouma2003@gmail.com>
2022-05-22 13:27:02 -0300

    Expressões e operadores

---
+++ javaScript/7_expressoes_operadores/operador_ternario.js
@@ -0,0 +1,14 @@
+// condition ? value1 : value2;
+let pao = true;
+let queijo = true;
+
+const niceBreakFast = pao && queijo ? 'Café top' : 'Café cancelado!';
+console.log(niceBreakFast);
+
+console.clear();
+
+// Consultando maioridade ou menor de idade
+const idade = 17;
+
+const maioridade = idade >= 18? 'Maioridade' : 'Ainda é menor de idade';
+console.log(maioridade);
---
+++ javaScript/7_expressoes_operadores/operadores_logicos.js
@@ -0,0 +1,12 @@
+let pao = true;
+let queijo = false;
+
+// Operador and
+console.log(pao && queijo);
+
+// Operador our
+console.log(pao || queijo);
+
+// Operador not 
+console.log(!pao);
+console.log(!queijo);
